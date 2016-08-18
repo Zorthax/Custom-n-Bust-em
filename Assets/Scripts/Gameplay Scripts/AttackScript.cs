dae02946a0fe95e0d904e4612fbb532afbcaeba3
Adam Schmidt <adam.schmidt@ad.aie.edu>
2016-08-18 16:59:44 +0930

    Double attacks and hitboxes

--- Assets/Scripts/Gameplay Scripts/AttackScript.cs
+++ Assets/Scripts/Gameplay Scripts/AttackScript.cs
@@ -8,6 +8,9 @@ public class AttackScript : MonoBehaviour {
     int frame = 0;
     public float animationSpeed = 0.005f;
     PolygonCollider2D localCollider;
+    bool secondAttack;
+    bool attackBuffer;
+    KeyCode attackType;
 
     [System.Serializable]
     public class Attack
@@ -16,6 +19,12 @@ public class AttackScript : MonoBehaviour {
         public PolygonCollider2D[] frameHitboxes;
         public int firstFrameOfAttack;
         public int lastFrameOfAttack;
+        public bool twoHits;
+        public float secondHitFrame;
+        public Sprite[] secondSprites;
+        public PolygonCollider2D[] secondFrameHitboxes;
+        public int secondFirstFrameOfAttack;
+        public int secondLastFrameOfAttack;
     }
     public Attack[] attackList;
 
@@ -25,9 +34,23 @@ public class AttackScript : MonoBehaviour {
     SpriteRenderer renderor;
     void Animation()
     {
-        renderor.sprite = currentAttack.sprites[Mathf.RoundToInt(spriteIndex)];
-        if (spriteIndex <= currentAttack.sprites.Length - 1) spriteIndex += animationSpeed;
-        else currentAttack = null;
+        Destroy(gameObject.GetComponent<PolygonCollider2D>());
+        if (!secondAttack) renderor.sprite = currentAttack.sprites[Mathf.RoundToInt(spriteIndex)];
+        else if (secondAttack) renderor.sprite = currentAttack.secondSprites[Mathf.RoundToInt(spriteIndex)];
+        if (secondAttack && spriteIndex <= currentAttack.secondSprites.Length - 1) //Second attack's sprites
+            spriteIndex += animationSpeed;
+        else if (!secondAttack && spriteIndex <= currentAttack.sprites.Length - 1) //First attack's sprites
+            spriteIndex += animationSpeed;
+        else if (!secondAttack && attackBuffer)
+        {
+            spriteIndex = 0;
+            secondAttack = true;
+        }
+        else
+        {
+            currentAttack = null;
+            secondAttack = false;
+        }
     }
 
     static Controls con;
@@ -47,12 +70,21 @@ public class AttackScript : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
     {
         if (currentAttack != null)
         {
+            NextAttack();
             Animation();
-            if (currentAttack != null) SetHitBox();
+            if (currentAttack != null)
+            {
+                SetHitBox();
+                
+            }
+            else
+            {
+                attackBuffer = false;
+            }
         }
         else
             GetComponentInParent<PlayerMovement>().action = false;
@@ -64,16 +96,31 @@ public class AttackScript : MonoBehaviour {
         {
             if (direction == 0) { if (onGround) currentAttack = attackList[neutralGround1]; }
             spriteIndex = 0;
+            secondAttack = false;
+            attackType = attackKey;
         }
     }
 
     void SetHitBox()
     {
-        if (spriteIndex >= currentAttack.firstFrameOfAttack && spriteIndex <= currentAttack.lastFrameOfAttack)
+        
+        localCollider = gameObject.AddComponent<PolygonCollider2D>();
+        localCollider.isTrigger = true;
+        if (secondAttack && spriteIndex >= currentAttack.secondFirstFrameOfAttack && spriteIndex <= currentAttack.secondLastFrameOfAttack)//Second attack's hitboxes
         {
-            localCollider.SetPath(0, currentAttack.frameHitboxes[Mathf.RoundToInt(spriteIndex)].GetPath(0));
+            localCollider.SetPath(0, currentAttack.secondFrameHitboxes[Mathf.RoundToInt(spriteIndex) - currentAttack.secondFirstFrameOfAttack].GetPath(0));
+            return;
+        }
+        if (!secondAttack && spriteIndex >= currentAttack.firstFrameOfAttack && spriteIndex <= currentAttack.lastFrameOfAttack) //First attack's hitboxes
+        {
+            localCollider.SetPath(0, currentAttack.frameHitboxes[Mathf.RoundToInt(spriteIndex) - currentAttack.firstFrameOfAttack].GetPath(0));
             return;
         }
         localCollider.pathCount = 0;
+    }
+    void NextAttack()
+    {
+        if (!secondAttack && spriteIndex >= currentAttack.secondHitFrame && Input.GetKeyDown(attackType))
+        { attackBuffer = true; }
     }
 }
